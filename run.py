917c81c0fc000f1fadd8f8cb6fb93f6b4d8ef218
chirsch <cecile.h.75@gmail.com>
2021-02-08 13:47:45 +0100

    create_category

--- run.py
+++ run.py
@@ -28,14 +28,40 @@ def index_categories():
 @app.route('/categories/<int:cat_id>')
 def show_category(cat_id):
     db = get_db()
-    cursor = db.execute("SELECT id, name FROM categories WHERE id = ?", [cat_id])
+    cursor = db.execute(
+        "SELECT id, name FROM categories WHERE id = ?", [cat_id]
+    )
     category = cursor.fetchone()
-    if category == None:
+    if category is None:
         abort(404)
     return jsonify({
         "id": category[0],
         "name": category[1]
     })
+
+
+@app.route('/categories', methods=['GET', 'POST'])
+def create_category():
+    db = get_db()
+    if request.method == "POST":
+        try:
+            name = request.form.get('name')
+            cursor = db.execute(
+                "INSERT INTO categories (name) values (?)", [name]
+            )
+            new_cat = db.commit()
+            cursor = db.execute(
+                "SELECT MAX(id), name FROM categories"
+            )
+            new_cat = cursor.fetchone()
+            if new_cat is None:
+                abort(404)
+            return jsonify({
+                "id": new_cat[0],
+                "name": new_cat[1]
+            })
+        except UnprocessableEntity:
+            abort(422)
 
 
 if __name__ == "__main__":
