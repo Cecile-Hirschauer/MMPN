ca81e0b38f8e93ec3c52230e4a7c38b2024792f2
chirsch <cecile.h.75@gmail.com>
2021-02-10 09:50:06 +0100

    delete_category 2

---
+++ requetes.sh
@@ -0,0 +1,8 @@
+curl -d "name=Water Games" -X POST  http://127.0.0.1:5000/categories
+curl -X DELETE http://127.0.0.1:5000/categories/8url http://127.0.0.1:5000/categories
+curl http://127.0.0.1:5000/categories
+
+# toys
+
+curl http://127.0.0.1:5000/toys
+curl http://127.0.0.1:5000/toys/2
